c45d766f0190e5210571be2579b7828de8024321
first0506 <first0506@naver.com>
2022-02-12 15:02:07 +0000

    Check the code against Python coding standards

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
-import json
+#import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,12 +16,16 @@ language_translator = LanguageTranslatorV3(
     authenticator=authenticator)
 language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com')
 
-def englishToFrench(englishText):
-    ## Translate
-    frenchText = language_translator.translate(text=englishText, model_id='en-fr').get_result()
-    return frenchText['translations'][0]['translation']
+def english_to_french(english_text):
+    '''
+    Translate English to French
+    '''
+    french_text = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    return french_text['translations'][0]['translation']
 
-def frenchToEnglish(frenchText):
-    ## Translate
-    englishText = language_translator.translate(text=frenchText, model_id='fr-en').get_result()
-    return englishText['translations'][0]['translation']
+def french_to_english(french_text):
+    '''
+    Translate French to English
+    '''
+    english_text = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    return english_text['translations'][0]['translation']
